b222ea7df701a0a01a06f3d7cbc4435a8ec041c5
anback <anders.back@me.com>
2017-11-22 13:46:02 +0100

    console.log({user, token})

--- createKeys.js
+++ createKeys.js
@@ -4,4 +4,5 @@ let token = process.env.GITHUB_TOKEN || 'No GITHUB_TOKEN env var plz check'
 if(!user) throw Error('Could not find GITHUB_USER in environment variables')
 if(!token) throw Error('Could not find GITHUB_TOKEN in environment variables')
 
+console.log({user, token})
 fs.writeFileSync('./src/keys.json', JSON.stringify({user, token}))
